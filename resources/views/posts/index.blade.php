702db2b1d48fa910a4367bda5baae9279d6a3b24
NozomiTachizawa <mugikko27@gmail.com>
2022-05-31 00:17:06 +0900

    08-2-4_notFinish

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,13 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-
         <title>Blog</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        
     </head>
+    <body>
         <h1>Blog Name</h1>
         <div class="posts">
             @foreach ($posts as $post)
@@ -18,6 +16,8 @@
                 </div>
             @endforeach
         </div>
-    <body>
+        <div class="paginate">
+            {{ $posts->links() }}
+        </div>
     </body>
 </html>
